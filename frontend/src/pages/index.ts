0e3089b694d0abf7cf3e6314bb5dfdefb9a57319
Tiago-0liveira <tfgoliveira@gmail.com>
2025-02-14 01:25:35 +0000

    added tailwindcss

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,8 +2,8 @@ import Router from "@/router/Router";
 
 const component = async () => {
 	const template = `
-		<div class="home">
-			<h1>Welcome Home</h1>
+		<div class="home h-40 flex flex-col justify-evenly">
+			<h1 class="">Welcome Home</h1>
 			<nav>
 				<a href="/about">About</a>
 				<a href="/contact">Contact</a>
