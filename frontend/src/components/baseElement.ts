b871c72d74b7727ab2998b2899d80120166a9686
adilsxn <24adilsonnunes@gmail.com>
2025-03-09 17:17:47 +0000

    add: baseElement + Navbar components

--- frontend/src/components/baseElement.ts
+++ frontend/src/components/baseElement.ts
@@ -1,1 +1,33 @@
+export class baseElement extends HTMLElement {
+	private ElemEventListeners: any;
+	private isRendered: boolean;
 
+	constructor() {
+		super();
+		this.isRendered = false;
+		this.ElemEventListeners = [];
+	}
+	connectedCallback(): void {
+		console.log("hello from connectedCallback");
+	}
+
+	disconnectedCallback(): void {
+		console.log("hello from disconnectedCallback");
+	}
+
+	attributeChangedCallback(): void {
+		this.updateRender();
+	}
+
+	render(): string {
+		return "";
+	}
+
+	updateRender(): string {
+		return this.render() + this.style;
+	}
+
+	getStyle(): string {
+		return "<style></style>";
+	}
+}
