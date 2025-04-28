0d4e3b2f1ab9e88d23a0a52007932c40cfe67cc3
adilsxn <24adilsonnunes@gmail.com>
2025-04-28 01:17:43 +0100

    add: new index page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -4,25 +4,15 @@ const component = async () => {
 	const loggedIn = Boolean(window.user);
 	const userElement = loggedIn
 		? '<a href="/user">My User</a>'
-		: '<a href="/login">Login with 42</a>';
+		: '<a href="/signUp">Login with 42</a>';
 
-	// <img src="42-logo.svg" class="h-8" alt="42School logo">
-	// <span class="self-center text-2xl ">Transcendence</span>
-	// <div class="flex space-x-7" id="navbar-left">
-
-	//TODO: Add
-	//@layer components {
-	//    .custom-container{
-	//      @apply tailwindcss classes
-	//      }
-	// } in order to isolate the styles we want to specifically
 	const template = `
     <nav class="bg-blue-600 text-white p-4 shadow-md">
         <div class="container mx-auto flex justify-between items-center">
             <a href="/" class="text-xl font-bold hover:text-purple-400">Transcendence</a>
             <div class="space-x-4">
                 <a href="/login" class="hover:text-purple-400">Sign In</a>
-                <a href="/register" class="hover:text-purple-400">Sign Up</a>
+                <a href="/signUp" class="hover:text-purple-400">Sign Up</a>
             </div>
         </div>
     </nav>
