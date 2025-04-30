4cbc312b8caf753e89656e4d9475f9d33078d47d
Tiago-0liveira <tfgoliveira@gmail.com>
2025-04-30 22:31:54 +0100

    (fix): authGuard redirects to login and takes the page the user wants to load so when the user logs in he is redirected to the original page he wanted to open

--- docs/fixes.md
+++ docs/fixes.md
@@ -6,5 +6,6 @@
 	- [x] on google oauth complete we should clear the http cookie used for completing the signUp
 
 ### Frontend
-	- [ ] in the auth middleware change from /user to the actual path the user wants to go to
+	- [x] in the auth middleware change from /user to the actual path the user wants to go to
 	- [ ] in Auth Manager check for all auth requests that aren't using this.authFetch, because they should!
+	- [ ] in Auth Manager authFetch instead of doing the request right away check at least if accessToken is valid especially for recovering the user on first page load (reduce 1 request)
--- frontend/src/router/guards.ts
+++ frontend/src/router/guards.ts
@@ -1,6 +1,5 @@
 import AuthManager from "@/auth/authManager";
 import Router from "@/router/Router"
-import { encodeURIforLogin } from "@/uri-encoding";
 
 const checkAuth: () => Promise<boolean> = async () => {
 	/*if (!AuthManager.getInstance().loggedIn)
@@ -13,10 +12,9 @@ const checkAuth: () => Promise<boolean> = async () => {
 // Authentication guard example
 export const authGuard: RouteGuard = async (to, _from) => {
 	const isAuthenticated = await checkAuth(); // Your auth check implementation
+	console.log("authGuard: ", to, _from);
 	if (!isAuthenticated) {
-		const uri = encodeURIforLogin("login", to);
-		console.log(uri);
-		Router.getInstance().navigate(uri);
+		Router.getInstance().navigate("/login", {}, { returnTo: Router.makeUrl(to.path, to.params, to.query) });
 		return false;
 	}
 	return true;
