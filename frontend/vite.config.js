afa48c073c074da235be4af6a3b5824e31fa3943
Tiago-0liveira <tfgoliveira@gmail.com>
2025-05-06 15:26:09 +0100

    (chore): added tailwind as dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,31 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-	tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-	  '@component': path.resolve(__dirname, './src/components/'),
-	  '@page': path.resolve(__dirname, './src/pages/'),
-      '@types': path.resolve(__dirname, './types'),
-    }
-  },
-  build: {
-    sourcemap: true,
-  },
-  server: {
-	watch: {
-		usePolling: true,
+	css: {
+		postcss: './postcss.config.cjs', // Optional if PostCSS is in the root
 	},
-    port: 3000,
-	strictPort: true,
-	historyApiFallback: true,
-	host: "0.0.0.0",
-	headers: {
-		"Cross-Origin-Opener-Policy": "same-origin-allow-popups"
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@component': path.resolve(__dirname, './src/components/'),
+			'@page': path.resolve(__dirname, './src/pages/'),
+			'@types': path.resolve(__dirname, './types'),
+		}
+	},
+	build: {
+		sourcemap: true,
+	},
+	server: {
+		watch: {
+			usePolling: true,
+		},
+		port: 3000,
+		strictPort: true,
+		historyApiFallback: true,
+		host: "0.0.0.0",
+		headers: {
+			"Cross-Origin-Opener-Policy": "same-origin-allow-popups"
+		}
 	}
-  }
 });
