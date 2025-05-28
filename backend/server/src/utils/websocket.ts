aa23642bd5a4c9e4ed56784aa5756b162e58a83b
adilsxn <24adilsonnunes@gmail.com>
2025-05-29 00:41:50 +0100

    add: newPrivateMessage handler

--- backend/server/src/utils/websocket.ts
+++ backend/server/src/utils/websocket.ts
@@ -1,26 +1,26 @@
 import type { RawData } from "ws";
-import type { WebSocket } from "@fastify/websocket"
+import type { WebSocket } from "@fastify/websocket";
 import { connectedSocketClients } from "@api/websocket";
 import Database from "@db/Database";
 
 export const processRawData = (data: RawData): string => {
 	if (Buffer.isBuffer(data)) {
-		return data.toString('utf-8');
+		return data.toString("utf-8");
 	} else if (data instanceof ArrayBuffer) {
 		const uint8Array = new Uint8Array(data);
-		return new TextDecoder('utf-8').decode(uint8Array);
+		return new TextDecoder("utf-8").decode(uint8Array);
 	} else if (Array.isArray(data) && data.every(Buffer.isBuffer)) {
 		const concatenatedBuffer = Buffer.concat(data);
-		return concatenatedBuffer.toString('utf-8');
+		return concatenatedBuffer.toString("utf-8");
 	} else {
 		throw new Error("Unsupported RawData format");
 	}
-}
+};
 
 /**
- * 
- * @param userId 
- * @param deviceId 
+ *
+ * @param userId
+ * @param deviceId
  * @returns `true` if user not found. `false` if user is found and deviceId does not match the stored one in the server
  */
 export const userCanLogIn = (userId: number, deviceId: string): boolean => {
@@ -31,24 +31,26 @@ export const userCanLogIn = (userId: number, deviceId: string): boolean => {
 		}
 	}
 	return true;
-}
+};
 
 export const websocketRegisterNewLogin = (userId: number, deviceId: string) => {
-	connectedSocketClients.set(userId, newDisconnectedClient(deviceId))
-}
+	connectedSocketClients.set(userId, newDisconnectedClient(deviceId));
+};
 
-export const isSocketValidMessage = (message: any): message is SocketMessage => {
-	return typeof message === "object" && typeof message.type === "string"
-}
+export const isSocketValidMessage = (
+	message: any,
+): message is SocketMessage => {
+	return typeof message === "object" && typeof message.type === "string";
+};
 
 const newDisconnectedClient = (deviceId: string): ClientValue => {
 	return {
 		socket: null,
 		connectedAt: 0,
 		connceted: false,
-		deviceId
+		deviceId,
 	} as const;
-}
+};
 
 export const updateDisconnectedClient = (userId: number, socket: WebSocket) => {
 	const clientValue = connectedSocketClients.get(userId);
@@ -56,7 +58,7 @@ export const updateDisconnectedClient = (userId: number, socket: WebSocket) => {
 	clientValue.connceted = true;
 	clientValue.connectedAt = Date.now();
 	clientValue.socket = socket;
-}
+};
 
 /**
  * @description notifications via websocket. they just work if the user recieving the notification is already connected to the websocket
@@ -66,20 +68,26 @@ export const notify = {
 	 * @description dispatches notifications for every friend so they now the user is now online!
 	 */
 	friendsOnline: async (userId: number) => {
-		const friendsWithInfo = await Database.getInstance().friendsTable.getFriendsWithInfo(userId, 0, 5000);
+		const friendsWithInfo =
+			await Database.getInstance().friendsTable.getFriendsWithInfo(
+				userId,
+				0,
+				5000,
+			);
 		if (friendsWithInfo.error) return;
 		friendsWithInfo.result.forEach((friend) => {
 			if (connectedSocketClients.has(friend.id)) {
-
 				const clientValue = connectedSocketClients.get(friend.id);
 				if (!clientValue) return;
-				clientValue.socket?.send(JSON.stringify({
-					type: "friend-online",
-					friendName: friend.displayName,
-					avatar: friend.avatarUrl
-				} satisfies SocketMessage))
+				clientValue.socket?.send(
+					JSON.stringify({
+						type: "friend-online",
+						friendName: friend.displayName,
+						avatar: friend.avatarUrl,
+					} satisfies SocketMessage),
+				);
 			}
-		})
+		});
 	},
 
 	/**
@@ -87,15 +95,21 @@ export const notify = {
 	 */
 	friendOnline: async (userId: number, friendId: number) => {
 		const connectedFriend = connectedSocketClients.get(friendId);
-		if (connectedFriend && connectedFriend.connceted && connectedFriend.socket) {
+		if (
+			connectedFriend &&
+			connectedFriend.connceted &&
+			connectedFriend.socket
+		) {
 			const dbRes = await Database.getInstance().userTable.getById(userId);
 			if (dbRes.error) return;
-			const user = dbRes.result
-			connectedFriend.socket.send(JSON.stringify({
-				type: "friend-online",
-				friendName: user.displayName,
-				avatar: user.avatarUrl
-			} satisfies SocketMessage))
+			const user = dbRes.result;
+			connectedFriend.socket.send(
+				JSON.stringify({
+					type: "friend-online",
+					friendName: user.displayName,
+					avatar: user.avatarUrl,
+				} satisfies SocketMessage),
+			);
 		}
 	},
 
@@ -104,15 +118,21 @@ export const notify = {
 	 */
 	friendAcceptedRequest: async (userId: number, friendId: number) => {
 		const connectedFriend = connectedSocketClients.get(friendId);
-		if (connectedFriend && connectedFriend.connceted && connectedFriend.socket) {
+		if (
+			connectedFriend &&
+			connectedFriend.connceted &&
+			connectedFriend.socket
+		) {
 			const dbRes = await Database.getInstance().userTable.getById(userId);
 			if (dbRes.error) return;
-			const user = dbRes.result
-			connectedFriend.socket.send(JSON.stringify({
-				type: "friend-request-accepted",
-				friendName: user.displayName,
-				avatar: user.avatarUrl
-			} satisfies SocketMessage))
+			const user = dbRes.result;
+			connectedFriend.socket.send(
+				JSON.stringify({
+					type: "friend-request-accepted",
+					friendName: user.displayName,
+					avatar: user.avatarUrl,
+				} satisfies SocketMessage),
+			);
 		}
 	},
 
@@ -121,16 +141,47 @@ export const notify = {
 	 */
 	friendRequest: async (userId: number, friendId: number) => {
 		const connectedFriend = connectedSocketClients.get(friendId);
-		if (connectedFriend && connectedFriend.connceted && connectedFriend.socket) {
+		if (
+			connectedFriend &&
+			connectedFriend.connceted &&
+			connectedFriend.socket
+		) {
 			const dbRes = await Database.getInstance().userTable.getById(userId);
 			if (dbRes.error) return;
-			const user = dbRes.result
-			connectedFriend.socket.send(JSON.stringify({
-				type: "friend-request",
-				friendName: user.displayName,
-				avatar: user.avatarUrl,
-				friendId: user.id,
-			} satisfies SocketMessage))
+			const user = dbRes.result;
+			connectedFriend.socket.send(
+				JSON.stringify({
+					type: "friend-request",
+					friendName: user.displayName,
+					avatar: user.avatarUrl,
+					friendId: user.id,
+				} satisfies SocketMessage),
+			);
 		}
 	},
-}
+
+	newPrivateMessage: async (
+		userId: number,
+		friendId: number,
+		message: string,
+	) => {
+		const connectedFriend = connectedSocketClients.get(friendId);
+		if (
+			connectedFriend &&
+			connectedFriend.connceted &&
+			connectedFriend.socket
+		) {
+			const dbRes = await Database.getInstance().userTable.getById(userId);
+			if (dbRes.error) return;
+			const user = dbRes.result;
+			connectedFriend.socket.send(
+				JSON.stringify({
+					type: "private-message",
+					friendName: user.displayName,
+					friendId: user.id,
+					payload: message,
+				} satisfies SocketMessage),
+			);
+		}
+	},
+};
