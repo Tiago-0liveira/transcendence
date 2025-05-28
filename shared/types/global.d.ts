1d6cfb11e0554a474a9f6bf8d8a86207c0957649
adilsxn <24adilsonnunes@gmail.com>
2025-05-29 00:42:08 +0100

    add: private message union element

--- shared/types/global.d.ts
+++ shared/types/global.d.ts
@@ -1,9 +1,19 @@
 type SocketMessageBase<T extends string, P extends object> = { type: T } & P;
 
-type SocketMessageType = SocketMessage["type"]
+type SocketMessageType = SocketMessage["type"];
 
-type SocketMessage = 
-	SocketMessageBase<"friend-online", { friendName: string, avatar: string }>
-	| SocketMessageBase<"friend-request", { friendName: string, avatar: string, friendId: number }>
-	| SocketMessageBase<"friend-request-accepted", { friendName: string, avatar: string }>
-
+type SocketMessage =
+	| SocketMessageBase<"friend-online", { friendName: string; avatar: string }>
+	| SocketMessageBase<
+			"friend-request",
+			{ friendName: string; avatar: string; friendId: number }
+	  >
+	| SocketMessageBase<
+			"friend-request-accepted",
+			{ friendName: string; avatar: string }
+	  >
+	| SocketMessageBase<
+			"private-message",
+			{ friendName: string; friendId: number; payload: string }
+	  >;
+// | SocketMessageBase<"room-message", { roomName: string; payload: string }>;
