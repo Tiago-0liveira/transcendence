95153c04bf132b5b6d0607cfd2c3cda395772a81
adilsxn <24adilsonnunes@gmail.com>
2025-06-12 00:32:47 +0100

    change: new irc message and irc notification types

--- shared/types/global.d.ts
+++ shared/types/global.d.ts
@@ -3,32 +3,34 @@ type SocketMessageBase<T extends string, P extends object> = { type: T } & P;
 type SocketMessageType = SocketMessage["type"];
 
 type SocketMessage =
-	| SocketMessageBase<"friend-online", { friendName: string; avatar: string }>
-	| SocketMessageBase<
-			"friend-request",
-			{ friendName: string; avatar: string; friendId: number }
-	  >
-	| SocketMessageBase<
-			"friend-request-accepted",
-			{ friendName: string; avatar: string }
-	  >
-	| SocketMessageBase<
-			"new-irc-notification",
-			{
-				// command: string;
-				// params: string[];
-				source?: number;
-				target: number;
-				// content: string;
-			}
-	  >
-	| SocketMessageBase<
-			"new-irc-message",
-			{
-				command: string;
-				params: string[];
-				source?: number;
-				target: number;
-				content: string;
-			}
-	  >;
+  | SocketMessageBase<"friend-online", { friendName: string; avatar: string }>
+  | SocketMessageBase<
+      "friend-request",
+      { friendName: string; avatar: string; friendId: number }
+    >
+  | SocketMessageBase<
+      "friend-request-accepted",
+      { friendName: string; avatar: string }
+    >
+  | SocketMessageBase<
+      "new-irc-notification",
+      {
+        // command: string;
+        // params: string[];
+        source?: number;
+        target: number;
+        // content: string;
+      }
+    >
+  | SocketMessageBase<
+      "new-irc-message",
+      {
+        command: string;
+        params: string[];
+        source?: number;
+        target: number;
+        content: string;
+        isPrivateMessage: boolean;
+        isChannelMessage: boolean;
+      }
+    >;
