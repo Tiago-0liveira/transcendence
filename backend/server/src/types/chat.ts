6d19ac85ba3805175e3bdfef28f050c998bccbea
adilsxn <24adilsonnunes@gmail.com>
2025-06-12 00:32:24 +0100

    add: irc room type, missing methods for creation and deletion and topical change or user removal

--- backend/server/src/types/chat.ts
+++ backend/server/src/types/chat.ts
@@ -1,24 +1,6 @@
-export interface IrcChannel {
+interface IrcRoom {
 	name: string;
 	topic: string;
-	user: Set<number>;
-	created: number;
+	members: Set<User>;
+	created: Date;
 }
-
-export interface IrcMessage {
-	type: "IRC_MESSAGE";
-	command: string;
-	params: string[];
-	source: string;
-	target: string;
-	content: string;
-}
-
-export type IrcCommand =
-	| "JOIN"
-	| "PRIVMSG"
-	| "NICK"
-	| "LIST"
-	| "TOPIC"
-	| "NAMES"
-	| "QUIT";
