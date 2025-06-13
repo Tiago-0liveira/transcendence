35fd2e7bf01cd9ee8f32efd4ae0060aafc26c941
tiagoliv <tfgoliveira@gmail.com>
2025-06-13 16:12:14 +0100

    (feat): backend url dynamically adapts to the connected ip on the frontend(localhost or 10.12.4.12

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,10 +1,10 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_PORT = import.meta.env.VITE_BACKEND_PORT;
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-
 console.log(import.meta.env)
 
-if (!BACKEND_URL)
-    throw new Error("VITE_BACKEND_URL is necessary in the .env file! Please define it")
+if (!BACKEND_PORT)
+    throw new Error("VITE_BACKEND_PORT is necessary in the .env file! Please define it")
+const BACKEND_URL = window.location.protocol + "//" +  window.location.hostname + ":" + BACKEND_PORT
 const GOOGLE_OAUTH_ENABLED = Boolean(GOOGLE_CLIENT_ID);
 if (!GOOGLE_OAUTH_ENABLED)
 {
