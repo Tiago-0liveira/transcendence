1a902ff520e23b67d62b4838692548eac7336d5c
adilsxn <24adilsonnunes@gmail.com>
2025-06-18 17:24:24 +0100

    add: chat page to the index page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -3,20 +3,20 @@ import Router from "@/router/Router";
 import { conditionalRender } from "@/utils/conditionalRender";
 
 const component = async () => {
-
 	const loggedIn = Boolean(AuthManager.getInstance().User);
 
-	const template = /* html */`
+	const template = /* html */ `
 		<div class="home flex-1 flex flex-col justify-normal">
 			<h1 class="text-black">Welcome Home</h1>
 			<div class="flex justify-center space-x-8">
 				<a href="/games/new-config" class="link">Games</a>
 				<a href="/games/rooms" class="link">Rooms</a>
 				<a href="/players" class="link">Players</a>	
+				<a href="/chat" class="link">Chat</a>	
 			</div>
 		</div>
 	`;
-	document.querySelector('#app')!.innerHTML = template;
-}
+	document.querySelector("#app")!.innerHTML = template;
+};
 
-Router.getInstance().register({ path: '/', component });
+Router.getInstance().register({ path: "/", component });
