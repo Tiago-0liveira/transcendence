525d66379f4df8930ecdc72941394e6b6c144b54
adilsxn <24adilsonnunes@gmail.com>
2025-06-19 20:25:29 +0100

    change: removing mentions chatComponent/2

--- frontend/src/components/registry.ts
+++ frontend/src/components/registry.ts
@@ -4,5 +4,3 @@ import "@component/LoadingSpinner";
 import "@component/RoomCard";
 import "@component/BracketCard";
 import "@component/UncompletedBracketCard";
-import "@component/chatComponent";
-import "@component/chatComponent2";
